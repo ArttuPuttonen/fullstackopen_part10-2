d1703458f2e8110a9e735c9edb21c50cac9e998b
Arttu Puttonen <arskaputtonen@gmail.com>
2024-07-22 17:29:12 +0300

    10.10

--- frontend/rate-repository-app/src/components/theme.js
+++ frontend/rate-repository-app/src/components/theme.js
@@ -1,3 +1,5 @@
+import Platform from 'react-native';
+
 const theme = {
     colors: {
       textPrimary: '#24292e',
@@ -10,8 +12,8 @@ const theme = {
       subheading: 16,
     },
     fonts: {
-      main: 'System',
-    },
+        main: Platform.OS === 'android' ? 'Roboto' : Platform.OS === 'ios' ? 'Arial' : 'System',
+      },
     fontWeights: {
       normal: '400',
       bold: '700',
